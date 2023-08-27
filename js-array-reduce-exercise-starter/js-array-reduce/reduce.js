994d207b3cb886628e0a421c74a791a505b0c53d
mjokipro <mjokipro@gmail.com>
2023-08-26 20:58:15 -0500

    ninth

--- js-array-reduce-exercise-starter/js-array-reduce/reduce.js
+++ js-array-reduce-exercise-starter/js-array-reduce/reduce.js
@@ -1,15 +1,18 @@
-/*
-Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
 
-Examples:
-    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
-    extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
-*/
+// Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
 
-function extractValue(arr, key) {
-    return arr.reduce((accum, val) => {
-        return val.
-    })
+// Examples:
+    const arr2 = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
+
+    // extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
+
+
+function extractValue(arr2, key) {
+
+    return arr2.reduce((accum, val) => {
+        accum.push(val[key]);
+        return accum;
+    }, []);
 }
 
 /*
@@ -23,37 +26,54 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    return str.split('').reduce((accum, letter) => {
+        let char = letter.toLowerCase();
+        if ('aeiou'.indexOf(char) !== -1) {
+            if (accum[char]) {
+                accum[char]++;
+            } else {
+                accum[char] = 1;
+            }
+        }
+        return accum;
+    }, {});
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
-
-Examples:
-    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
-    
-    addKeyAndValue(arr, 'title', 'Instructor') // 
-      [
-        {title: 'Instructor', name: 'Elie'}, 
-        {title: 'Instructor', name: 'Tim'}, 
-        {title: 'Instructor', name: 'Matt'}, 
-        {title: 'Instructor', name: 'Colt'}
-       ]
 */
+// Examples:
+    // const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
+    
+    // addKeyAndValue(arr, 'title', 'Instructor') 
+    //   [
+    //     {title: 'Instructor', name: 'Elie'}, 
+    //     {title: 'Instructor', name: 'Tim'}, 
+    //     {title: 'Instructor', name: 'Matt'}, 
+    //     {title: 'Instructor', name: 'Colt'}
+    //    ]
 
-function addKeyAndValue(arr, key, value) {}
+
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce((accum, val, i) => {
+        accum[i][key] = value;
+        return accum;
+    }, arr);
+}
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
 
 Examples:
+*/   
     
     function isEven(val){
         return val % 2 === 0;
     }
-    
     const arr = [1,2,3,4,5,6,7,8];
     
-    partition(arr, isEven) // [[2,4,6,8], [1,3,5,7]];
+    // partition(arr, isEven) // [[2,4,6,8], [1,3,5,7]];
     
     function isLongerThanThreeCharacters(val){
         return val.length > 3;
@@ -61,7 +81,16 @@ Examples:
     
     const names = ['Elie', 'Colt', 'Tim', 'Matt'];
     
-    partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
-*/
+    // partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
+function partition(arr, callback) { 
+    return arr.reduce((accum, nextVal) => {
+        if (callback(nextVal)) {
+            accum[0].push(nextVal);
+        } else {
+            accum[1].push(nextVal)
+        }
+        return accum;
+    }, [[], []]);
+}
 
-function partition(arr, callback) {}
+
