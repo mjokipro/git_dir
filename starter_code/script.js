bd7a024d47f4bcce31df9aba05fcccaadd88bfd8
mjokipro <mjokipro@gmail.com>
2023-09-09 18:12:39 -0500

    jquery

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,43 +4,35 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	let results = [];
-	if (str) {
-		results.filter(v => v.toLowerCase().includes(str));
-	}
-	return results;
-
+	return str.toLowerCase().split('')
+		.filter(v => fruits.indexOf(v) === -1).join('');
 }
 
 function searchHandler(e) {
 	e.preventDefault();
-	let inputVal = e.target.value.toLowerCase();
-	console.log(inputVal);
-	let results = search(inputVal);
-	showSuggestions(results,);
+	let targ = e.target.value;
+	if (!targ) return;
+	let sResult = search(targ);
+	showSuggestions(sResult, targ);
 }
 
-function showSuggestions(results,) {
+function showSuggestions(results, inputVal) {
+	// console.log(inputVal);
+	for (let result of results) {
+		let li = document.createElement('li');
+		li.innerHTML = `<li>${result}</li>`;
+		suggestions.appendChild(li);
+		// suggestions.append(li);
+	}
 	
-	let newArr = results.map(v => v = `<li>${v}</li>`);
-	suggestions.append(newArr);
+	// li.innerHTML = results.map(v => v);
+		// results.filter(v => v === inputVal)[0];
+	// suggestions.innerHTML = results.map(v => v);
+	
 }
-
-
-	
-
-	// if (!inputVal) suggestions.innerHTML = '';
-
-	// suggestions.innerHTML = newArr;
-	// suggestions.append(li);
-
-
 
 function useSuggestion(e) {
-	e.preventDefault();
-	// input.value = e.target;
-	//  suggestions.innerHTML = e.target.value;
-	// input.value = e.target.value;
+	// TODO
 }
 
 input.addEventListener('keyup', searchHandler);
--- starter_code/script2.js
+++ starter_code/script2.js
@@ -4,15 +4,13 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	let result = [];
-	
-	str.split('')
-	return results;
+	return str.toLowerCase().split('')
+		.filter(v => fruits.indexOf(v) === -1).join('');
 }
 
 function searchHandler(e) {
 	e.preventDefault();
-	let targ = e.target.value.toLowerCase();
+	let targ = e.target.value;
 	if (!targ) return;
 	let sResult = search(targ);
 	showSuggestions(sResult, targ);
@@ -21,8 +19,8 @@ function searchHandler(e) {
 function showSuggestions(results, inputVal) {
 	// console.log(inputVal);
 	let li = document.createElement('li');
-	li.innerHTML = results, inputVal;
-	console.log(li);
+	li.innerHTML = results.filter(v => v === inputVal)[0];
+	// suggestions.appendChild(li);
 	
 }
 
