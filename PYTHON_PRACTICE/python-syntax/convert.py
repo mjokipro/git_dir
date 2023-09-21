458812199dccaee793f0f0bcb9505678c204dfaf
mjokipro <mjokipro@gmail.com>
2023-09-20 19:18:23 -0500

    ano1

--- PYTHON_PRACTICE/python-syntax/convert.py
+++ PYTHON_PRACTICE/python-syntax/convert.py
@@ -14,8 +14,21 @@ def convert_temp(unit_in, unit_out, temp):
       convert_temp("c", "f", 0)  =>  32.0
       convert_temp("f", "c", 212) => 100.0
     """
+    if unit_in != "f" and unit_in != "c":
+        return f"Invalid unit {unit_in}"
 
-    # YOUR CODE HERE
+    if unit_out != "f" and unit_out != "c":
+        return f"Invalid unit {unit_out}"
+
+    if unit_in == "f" and unit_out == "c":
+        temp = (temp - 32) / 9 * 5
+
+    if unit_in == "c" and unit_out == "f":
+        temp = (temp * 5 / 9) + 32
+
+    return temp    
+
+    #f=c*1.8+32
 
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
