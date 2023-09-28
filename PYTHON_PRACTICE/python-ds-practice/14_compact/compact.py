463d120b0b0907e8cc58c6d5f9fecb6feb18ce54
mjokipro <mjokipro@gmail.com>
2023-09-28 17:27:01 -0500

    ano

--- PYTHON_PRACTICE/python-ds-practice/14_compact/compact.py
+++ PYTHON_PRACTICE/python-ds-practice/14_compact/compact.py
@@ -4,13 +4,4 @@ def compact(lst):
         >>> compact([0, 1, 2, '', [], False, (), None, 'All done'])
         [1, 2, 'All done']
     """
-    true_list = []
-    false_list = []
-
-    for val in lst:
-        if fn(val):
-            true_list.append(val)
-        else:
-            false_list.append(val)
-
-    return [true_list, false_list]
+    return [val for val in lst if val]
