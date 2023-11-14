4478dadb24e76c821bfc67d5da97139e90ba240d
mjokipro <mjokipro@gmail.com>
2023-11-13 18:07:39 -0600

    blogly tests

--- sql-2-flask-wtforms-demo/VideoDemo/app.py
+++ sql-2-flask-wtforms-demo/VideoDemo/app.py
@@ -21,56 +21,7 @@ def home_page():
     return render_template("home.html")
 
 
-@app.route('/phones')
-def list_phones():
-    """Renders directory of employees and phone numbers  (from dept)"""
-    emps = Employee.query.all()
-    return render_template('phones.html', emps=emps)
 
 
-@app.route('/snacks/new', methods=["GET", "POST"])
-def add_snack():
-    """Renders snack form (GET) or handles snack form submission (POST)"""
-    form = AddSnackForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        price = form.price.data
-        flash(f"Created new snack: name is {name}, price is ${price}")
-        return redirect('/')
-    else:
-        return render_template("add_snack_form.html", form=form)
-
-
-@app.route('/employees/new', methods=["GET", "POST"])
-def add_employee():
-    form = EmployeeForm()
-    depts = db.session.query(Department.dept_code, Department.dept_name)
-    form.dept_code.choices = depts
-    if form.validate_on_submit():
-        name = form.name.data
-        state = form.state.data
-        dept_code = form.dept_code.data
-
-        emp = Employee(name=name, state=state, dept_code=dept_code)
-        db.session.add(emp)
-        db.session.commit()
-        return redirect('/phones')
-    else:
-        return render_template('add_employee_form.html', form=form)
-
-
-@app.route('/employees/<int:id>/edit', methods=["GET", "POST"])
-def edit_employee(id):
-    emp = Employee.query.get_or_404(id)
-    form = EmployeeForm(obj=emp)
-    depts = db.session.query(Department.dept_code, Department.dept_name)
-    form.dept_code.choices = depts
-
-    if form.validate_on_submit():
-        emp.name = form.name.data
-        emp.state = form.state.data
-        emp.dept_code = form.dept_code.data
-        db.session.commit()
-        return redirect('/phones')
-    else:
-        return render_template("edit_employee_form.html", form=form)
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- sql-flask-blogly/test_flask.py
+++ sql-flask-blogly/test_flask.py
@@ -32,24 +32,11 @@ class UserViewsTestCase(TestCase):
         db.session.add(user)
         db.session.commit()
 
-        tag = Tag(name="TestTag")
-        db.session.add(tag)
-        db.session.commit()
-        
-        post = Post(title="TestTitle", content="TestContent", user=user, tags=[tag])
-        db.session.add(post)
-        db.session.commit()
         # # tag_ids = [int(num) for num in request.form.getlist("tags")]
         # # tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
 
-
-        
         self.user_id = user.id
         self.user = user
-        
-        self.tag = tag
-        
-        self.post = post
 
     def tearDown(self):
         """Clean up any fouled transaction."""
@@ -71,10 +58,79 @@ class UserViewsTestCase(TestCase):
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            # look for 'TestPet' created in setUp()
+            # Look in
+            self.assertIn('<h1>Create a user</h1>', html)
 
+    def test_new_users_post(self):
+        """test post"""
+        with app.test_client() as client:
+            
+            resp = client.post("/users/new",
+                data={"first_name": "Test123", "last_name":"Test321"})
+            html = resp.get_data(as_text=True)
 
-    # # def test_post(self):
-    # #    """test post"""
+            self.assertEqual(resp.status_code, 400)
+            # Look in
+
+    def test_users_show(self):
+        """Test show."""
+        with app.test_client() as client:
+
+            user = User.query.all()
+            
+            resp = client.get(f"/users/{ user[0].id }")
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<button class="btn btn-primary"', html)
+            
+    def test_users_edit_show(self):
+        """Test users edit page."""
+        with app.test_client() as client:
         
+            user = User.query.all()
+            
+            resp = client.get(f"/users/{ user[0].id }/edit")
+            html = resp.get_data(as_text=True)
         
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn(f'<h1>Edit Test123 Test321</h1>', html)
+           
+    def test_users_post_new(self):
+        """Test users new post page."""
+        with app.test_client() as client:
+            
+            user = User.query.all()
+            tags = Tag.query.all()
+            
+            resp = client.get(f"/users/{ user[0].id }/posts/new")
+            html = resp.get_data(as_text=True)
+        
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn(f'<i>Test123 Test321</i>', html)
+           
+    def test_tags(self):
+        """Test tags index page."""
+        with app.test_client() as client:
+            
+            tag = Tag.query.all()
+            
+            resp = client.get("/tags")
+            html = resp.get_data(as_text=True)
+        
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn(f'<a class="btn btn-primary"', html)
+
+    def test_tags_new(self):
+        """Test tags new page."""
+        with app.test_client() as client:
+            
+            tag = Tag.query.all()
+            
+            resp = client.get("/tags/new")
+            html = resp.get_data(as_text=True)
+        
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn(f'<h1>Create A Tag</h1>', html)
+           
+    
