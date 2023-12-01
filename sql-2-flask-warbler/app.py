0b33d6b40b0771335cd33b890b9804321e053ddf
mjokipro <mjokipro@gmail.com>
2023-11-30 20:34:06 -0600

    warb

--- sql-2-flask-warbler/app.py
+++ sql-2-flask-warbler/app.py
@@ -23,7 +23,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
-
+# db.create_all()
 
 ##############################################################################
 # User signup/login/logout
