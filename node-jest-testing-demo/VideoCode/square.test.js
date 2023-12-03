2418d9956a345798164f24109cc234385c1d1f9d
mjokipro <mjokipro@gmail.com>
2023-12-03 16:09:18 -0600

    test

--- node-jest-testing-demo/VideoCode/square.test.js
+++ node-jest-testing-demo/VideoCode/square.test.js
@@ -1,22 +1,23 @@
-const { square, cube } = require('./square');
+const { square, cube } = require('./square')
 
-describe('square function', function () {
-  test('square should square a number', function () {
+describe('square function', function(){
+  test('square should square a number', function(){
     const res = square(3)
     expect(res).toEqual(9)
   })
-
-  test('square should square negative numbers', function () {
-    const num = square(-9);
-    expect(num).toEqual(81)
+  
+  test('square neg num', function(){
+    
+    const res = square(-3)
+    expect(res).toEqual(9)
   })
 })
 
-describe('cube function', function () {
-  test('should cube a positive number', function () {
-    const num = cube(3);
+describe('cube function', function(){
+  test('cube positive num', function(){
+    const num = cube(3)
     expect(num).toEqual(27)
-    const num2 = cube(2);
+    const num2 = cube(2)
     expect(num2).toEqual(8)
   })
 })
