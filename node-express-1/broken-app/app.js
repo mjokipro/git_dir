60da7c7faae0aa0420ab716aa68f442761d2439a
mjokipro <mjokipro@gmail.com>
2023-12-11 13:03:55 -0600

    express eval

--- node-express-1/broken-app/app.js
+++ node-express-1/broken-app/app.js
@@ -1,22 +1,33 @@
 
 const express = require("express")
 const app = express();
+const request = require('request');
 
 // const ExpressError = require("./expressError")
 
 app.use(express.json());
 
-app.post("/", (req, res) => {
+// app.post("/", (req, res) => {
+//   let developers = req.body.developers
+//   let devs = getGit()
+//   return res.json(devs)
+// })
+
+app.post('/', function(req, res) {
   let developers = req.body.developers
-  let devs = getGit()
-  return res.json(devs)
-})
-/** 404 handler */
+  request(`https://api.github.com/users?${developers}`, function(error, response, body) {
+    console.log(body)
+  });
+
+  res.json(developers)
+});
 
 async function getGit(){
   let results = await $.getJSON(`https://api.github.com/users`);
   return results
 }
+
+/** 404 handler */
 // app.use(function (req, res, next) {
 //   return new ExpressError("Not Found", 404);
 // });
