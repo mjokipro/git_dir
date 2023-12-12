15e3a9dbc1ccee7b7b56f0aba9853defefbb7cf8
mjokipro <mjokipro@gmail.com>
2023-12-12 12:05:39 -0600

    anot

--- express-pg-intro-demo-copy/simple/routes/users.js
+++ express-pg-intro-demo-copy/simple/routes/users.js
@@ -10,138 +10,17 @@ const router = express.Router();
 
 /** Get users: [user, user, user] */
 
-router.get("/all", function (req, res, next) {
-  const results = db.query(
-        `SELECT * FROM users`);
+// router.get("/all", function (req, res, next) {
+//   const results = db.query(
+//         `SELECT * FROM usersdb`);
 
-  return res.json(results.rows);
-});
+//   return res.json(results.rows);
+// });
 
 
 /** (Fixed) Get users: [user, user, user] */
 
-router.get("/", async function (req, res, next) {
-  try {
-    const results = await db.query(
-          `SELECT id, name, type FROM users`);
 
-    return res.json(results.rows);
-  }
-
-  catch (err) {
-    return next(err);
-  }
-});
-
-
-// this version has security holes --- you could inject SQL
-// because the input isn't sanitized!
-
-/** Search by user type. */
-
-router.get("/search", async function (req, res, next) {
-  try {
-    const type = req.query.type;
-    
-    const results = await db.query(
-      `SELECT id, name, type 
-       FROM users
-       WHERE type='${type}'`);
-
-    return res.json(results.rows);
-  }
-
-  catch (err) {
-    return next(err);
-  }
-});
-
-
-// fixed version that uses parameterized query
-
-// (Fixed) Search by user type. */
-
-router.get("/good-search",
-      async function (req, res, next) {
-  try {
-    const type = req.query.type;
-
-    const results = await db.query(
-      `SELECT id, name, type 
-       FROM users
-       WHERE type=$1`, [type]);
-
-    return res.json(results.rows);
-  }
-
-  catch (err) {
-    return next(err);
-  }
-});
-
-
-
-/** Create new user, return user */
-
-router.post("/", async function (req, res, next) {
-  try {
-    const { name, type } = req.body;
-
-    const result = await db.query(
-          `INSERT INTO users (name, type) 
-           VALUES ($1, $2)
-           RETURNING id, name, type`,
-        [name, type]
-    );
-
-    return res.status(201).json(result.rows[0]);
-  }
-
-  catch (err) {
-    return next(err);
-  }
-});
-
-
-/** Update user, returning user */
-
-router.patch("/:id", async function (req, res, next) {
-  try {
-    const { name, type } = req.body;
-
-    const result = await db.query(
-          `UPDATE users SET name=$1, type=$2
-           WHERE id = $3
-           RETURNING id, name, type`,
-        [name, type, req.params.id]
-    );
-
-    return res.json(result.rows[0]);
-  }
-
-  catch (err) {
-    return next(err);
-  }
-});
-
-
-/** Delete user, returning {message: "Deleted"} */
-
-router.delete("/:id", async function (req, res, next) {
-  try {
-    const result = await db.query(
-        "DELETE FROM users WHERE id = $1",
-        [req.params.id]
-    );
-
-    return res.json({message: "Deleted"});
-  }
-
-  catch (err) {
-    return next(err);
-  }
-});
-// end
 
 
 module.exports = router;
