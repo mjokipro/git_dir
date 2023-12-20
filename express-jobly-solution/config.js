44333acd8514b4b8379f21a23fad505b5235c1f3
mjokipro <mjokipro@gmail.com>
2023-12-20 14:00:59 -0600

    alot

--- express-jobly-solution/config.js
+++ express-jobly-solution/config.js
@@ -12,8 +12,8 @@ const PORT = +process.env.PORT || 3001;
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "postgresql://postgres:newpassword@localhost/jobly_test"
-      : process.env.DATABASE_URL || "postgresql://postgres:newpassword@localhost/jobly";
+      ? "postgresql:///jobly_test"
+      : process.env.DATABASE_URL || "postgresql:///jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
