9b600306f59ecb77562af24f2784acad336f9123
mjokipro <mjokipro@gmail.com>
2023-12-20 16:00:31 -0600

    ban

---
+++ node-express-2/bankly/data.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS bankly;
+CREATE DATABASE bankly;
+
+\c bankly
+
+
+CREATE TABLE users (
+    username text PRIMARY KEY,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    email text NOT NULL,
+    phone text NOT NULL,
+    password text NOT NULL,
+    admin boolean DEFAULT false
+);
+
