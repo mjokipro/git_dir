bbde4ce27a996c4543b6822f9c1a3659e28bc799
mjokipro <mjokipro@gmail.com>
2024-01-08 14:10:45 -0600

    poke

--- react-cards-pokemon/src/PlayingCard.js
+++ react-cards-pokemon/src/PlayingCard.js
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
+import { useFlip } from "./hooks";
 import backOfCard from "./back.png";
-import "./PlayingCard.css"
+import "./PlayingCard.css";
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useState(true);
-  const flipCard = () => {
-    setIsFacingUp(isUp => !isUp);
-  };
+  const [isFacingUp, flip] = useFlip();
   return (
     <img
       src={isFacingUp ? front : back}
       alt="playing card"
-      onClick={flipCard}
+      onClick={flip}
       className="PlayingCard Card"
     />
   );
--- react-cards-pokemon/src/PokeDex.js
+++ react-cards-pokemon/src/PokeDex.js
@@ -1,39 +1,28 @@
-import React, { useState } from "react";
-import {v1 as uuid} from "uuid";
-import axios from "axios";
+import React from "react";
+import { useAxios } from "./hooks";
 import PokemonSelect from "./PokemonSelect";
 import PokemonCard from "./PokemonCard";
 import "./PokeDex.css";
 
-/* Renders a list of pokemon cards.
- * Can also add a new card at random,
+/* Renders a list of playing cards.
+ * Can also remove all cards,
+ * and add a new card, either at random
  * or from a dropdown of available pokemon. */
 function PokeDex() {
-  const [pokemon, setPokemon] = useState([]);
-  const addPokemon = async name => {
-    const response = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${name}/`
-    );
-    setPokemon(pokemon => [...pokemon, { ...response.data, id: uuid() }]);
-  };
+  const [pokemon, addPokemon, clearPokemon] = useAxios(
+    "pokemon",
+    "https://pokeapi.co/api/v2/pokemon/"
+  );
   return (
     <div className="PokeDex">
       <div className="PokeDex-buttons">
         <h3>Please select your pokemon:</h3>
         <PokemonSelect add={addPokemon} />
+        <button onClick={clearPokemon}>Delete the pokemon!</button>
       </div>
       <div className="PokeDex-card-area">
-        {pokemon.map(cardData => (
-          <PokemonCard
-            key={cardData.id}
-            front={cardData.sprites.front_default}
-            back={cardData.sprites.back_default}
-            name={cardData.name}
-            stats={cardData.stats.map(stat => ({
-              value: stat.base_stat,
-              name: stat.stat.name
-            }))}
-          />
+        {pokemon.map(card => (
+          <PokemonCard key={card.id} {...card} />
         ))}
       </div>
     </div>
