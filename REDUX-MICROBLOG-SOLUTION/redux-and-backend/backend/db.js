d92af77bdf06c09f622a8a3b889e939ed4f6533b
mjokipro <mjokipro@gmail.com>
2024-02-09 21:01:21 -0600

    adsf

--- REDUX-MICROBLOG-SOLUTION/redux-and-backend/backend/db.js
+++ REDUX-MICROBLOG-SOLUTION/redux-and-backend/backend/db.js
@@ -2,7 +2,7 @@
 
 const { Client } = require("pg");
 
-const client = new Client(process.env.DATABASE_URL || "postgresql:///microblog");
+const client = new Client(process.env.DATABASE_URL || "postgresql://postgres:newpassword@localhost/microblog");
 
 client.connect();
 
