fed0d55749807b3cb05b2f42a7de504b8137e221
mjokipro <mjokipro@gmail.com>
2024-02-12 11:57:26 -0600

    pizza updates

--- BACKEND-CHALLENGE-PIZZA/backend/models/order.js
+++ BACKEND-CHALLENGE-PIZZA/backend/models/order.js
@@ -8,9 +8,9 @@ const {
   UnauthorizedError,
 } = require("../expressError");
 
-  /** Find all pizzas.
+  /** Find all orders.
    *
-   * Returns [{ username, first_name, last_name, email, is_admin }, ...]
+   * Returns [{ user_id, total_items, total_price }, ...]
    **/
 
 class Order {
@@ -26,7 +26,48 @@ class Order {
 
     return result.rows;
   }
+
+// static async findAll({ minSalary, hasEquity, title } = {}) {
+//     let query = `SELECT j.id,
+//                         j.title,
+//                         j.salary,
+//                         j.equity,
+//                         j.company_handle AS "companyHandle",
+//                         c.name AS "companyName"
+//                  FROM jobs j 
+//                    LEFT JOIN companies AS c ON c.handle = j.company_handle`;
+//     let whereExpressions = [];
+//     let queryValues = [];
+
+//     // For each possible search term, add to whereExpressions and
+//     // queryValues so we can generate the right SQL
+
+//     if (minSalary !== undefined) {
+//       queryValues.push(minSalary);
+//       whereExpressions.push(`salary >= $${queryValues.length}`);
+//     }
+
+//     if (hasEquity === true) {
+//       whereExpressions.push(`equity > 0`);
+//     }
+
+//     if (title !== undefined) {
+//       queryValues.push(`%${title}%`);
+//       whereExpressions.push(`title ILIKE $${queryValues.length}`);
+//     }
+
+//     if (whereExpressions.length > 0) {
+//       query += " WHERE " + whereExpressions.join(" AND ");
+//     }
+
+//     // Finalize query and return results
+
+//     query += " ORDER BY title";
+//     const jobsRes = await db.query(query, queryValues);
+//     return jobsRes.rows;
+//   }
 }
+
 
 
 module.exports = Order
--- BACKEND-CHALLENGE-PIZZA/backend/routes/orders.js
+++ BACKEND-CHALLENGE-PIZZA/backend/routes/orders.js
@@ -11,6 +11,12 @@ const Order = require("../models/order");
 
 const router = express.Router();
 
+    /** GET / =>
+ *   { orders: [ { id, user_id, total_items, total_price }, ...] }
+ *
+ * Authorization required: none
+ */
+
 router.get("/", ensureCorrectUserOrAdmin, async function (req, res, next) {
     try {
       const orders = await Order.findAll();
@@ -20,6 +26,31 @@ router.get("/", ensureCorrectUserOrAdmin, async function (req, res, next) {
     }
   });
 
+    /** GET / =>
+ *   { orders: [ { id, user_id, total_items, total_price }, ...] }
+ *
+ * Authorization required: none
+ */
+
+// router.get("/", async function (req, res, next) {
+    // const q = req.query;
+    // arrive as strings from querystring, but we want as int/bool
+    // if (q.minSalary !== undefined) q.minSalary = +q.minSalary;
+    // q.hasEquity = q.hasEquity === "true";
+  
+    // try {
+    //   const validator = jsonschema.validate(q, jobSearchSchema);
+    //   if (!validator.valid) {
+    //     const errs = validator.errors.map(e => e.stack);
+    //     throw new BadRequestError(errs);
+    //   }
+  
+//       const orders = await Order.findAll();
+//       return res.json({ orders });
+//     } catch (err) {
+//       return next(err);
+//     }
+//   });
 
 
 module.exports = router;
